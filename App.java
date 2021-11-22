2d77c4d1db0e0f552fd4111258cdfd4b4e84bcac
Josseane Vendetti <93625811+j-josy@users.noreply.github.com>
2021-11-22 13:39:20 -0300

    commitei

---
+++ App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args){
+        Jogo jogo = new jogo();
+        
+jogo.jogo();
+
+    
+
+    }
+}
---
+++ Jogo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Jogo {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite um numero de 1 até 3");
+        System.out.println("1-Pedra");
+        System.out.println("2-Papel");
+        System.out.println("3-Tesoura");
+        
+        int jogadorHumano = 0;
+        int jogadorComputador = 0;
+
+        jogadorHumano = teclado.nextInt();
+        if(jogadorHumano == 1 && jogadorComputador == 2);
+        
+            if(jogadorComputador == 2);
+        
+            if(jogadorHumano == 2 && jogadorComputador == 2);
+            System.out.println("Digite o empate");
+
+        }
